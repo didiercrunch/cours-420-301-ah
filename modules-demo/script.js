4b97a2b1473225c41c95f15ea246d72fde587578
didier amyot <didier.amyot@gmail.com>
2023-12-01 20:26:23 -0500

    going somewhere.

---
+++ modules-demo/script.js
@@ -0,0 +1,10 @@
+import {average} from "https://cdn.jsdelivr.net/gh/didiercrunch/cours-420-301-ah@main/modules-examples/stats.js";
+
+
+function sayHello(){
+    alert(average([10, 67, 82, 109]));
+}
+
+
+document.getElementById("hello-button")
+    .addEventListener('click', sayHello);
